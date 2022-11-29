6ecb6025d82968cad8fcdf3b540af448fc426358
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-11-29 18:26:36 +0530

    5 files modified

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { PRODUCTS_API_URL } from '../../helpers/config';
 import { getJson } from '../../helpers/helperFns';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import './ProductPage.scss';
 
 const ProductPage = () => {
   const [state, setState] = useState({
@@ -30,8 +32,42 @@ const ProductPage = () => {
     })();
   }, []);
 
-  console.log(state);
-  return null;
+  const { isLoading, products, isError } = state;
+  console.log(products);
+  return (
+    <section className="product__page">
+      <Container>
+        <Row>
+          {products &&
+            products.map((p) => {
+              return (
+                <Col md={3} key={p.id}>
+                  <Card>
+                    <Card.Img variant="top" src={p.thumbnail} />
+                    <Card.Body>
+                      <Card.Title>{p.title}</Card.Title>
+                      <Card.Text>
+                        <strong>$</strong>
+                        {p.price}
+                      </Card.Text>
+                      <Button variant="dark">
+                        <i className="icofont icofont-shopping-cart"></i>
+                      </Button>
+                      <Button variant="warning">
+                        <i className="icofont icofont-heart-alt"></i>
+                      </Button>
+                      <Button variant="danger">
+                        <i className="icofont icofont-ui-delete"></i>
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default ProductPage;
