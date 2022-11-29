54e2257dddce71bf818bcdd431e863fddf453d1b
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-11-29 17:14:48 +0530

    2 files modified

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,1 +1,2 @@
 export const TIMEOUT = 10;
+export const PRODUCTS_API_URL = 'https://dummyjson.com/products';
--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,8 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { PRODUCTS_API_URL } from '../../helpers/config';
 import { getJson } from '../../helpers/helperFns';
 
-
 const ProductPage = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    products: [],
+    isError: null,
+  });
+
+  useEffect(() => {
+    setState((prev) => {
+      return { ...prev, isLoading: true };
+    });
+
+    (async () => {
+      try {
+        const data = await getJson(PRODUCTS_API_URL);
+        const productsData = [...data.products];
+
+        setState((prev) => {
+          return { ...prev, products: productsData, isLoading: false };
+        });
+      } catch (err) {
+        setState((prev) => {
+          return { ...prev, isError: err, isLoading: false };
+        });
+      }
+    })();
+  }, []);
+
+  console.log(state);
   return null;
 };
 
